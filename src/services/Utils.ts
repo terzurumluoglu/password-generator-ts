b712e34e70923f74a72cc901b908ce84e4a6e484
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-08-23 23:50:08 +0300

    prettier

--- src/services/Utils.ts
+++ src/services/Utils.ts
@@ -1,18 +1,17 @@
 export class Utils {
-  
-    static #instance: Utils;
-  
-    static get(): Utils {
-      if (!this.#instance) {
-        this.#instance = new Utils();
-      }
-      return this.#instance;
-    }
-  
-    generateEmptyArray = (length: number) => [...Array(length).keys()];
 
-    generateRandomNumber = (max: number, min = 0) => Math.floor(Math.random() * (max - min + 1) - min);
-  
-    shuffle = (arr: string[]): string[] => arr.sort(() => +Math.random - 0.5);
+  static #instance: Utils;
+
+  static get(): Utils {
+    if (!this.#instance) {
+      this.#instance = new Utils();
+    }
+    return this.#instance;
   }
-  
+
+  generateEmptyArray = (length: number) => [...Array(length).keys()];
+
+  generateRandomNumber = (max: number, min = 0) => Math.floor(Math.random() * (max - min + 1) - min);
+
+  shuffle = (arr: string[]): string[] => arr.sort(() => +Math.random - 0.5);
+}
