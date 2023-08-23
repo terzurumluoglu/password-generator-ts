98d02d5a4f044e9425e6d6d8d68612f5015d138d
Toprak Erzurumluoğlu <toprakerzurumluoglu@hotmail.com>
2023-08-23 23:50:29 +0300

    new function for selected options

--- src/services/Utils.ts
+++ src/services/Utils.ts
@@ -1,3 +1,5 @@
+import { IPasswordConfig } from "../models/IPasswordConfig";
+
 export class Utils {
 
   static #instance: Utils;
@@ -14,4 +16,8 @@ export class Utils {
   generateRandomNumber = (max: number, min = 0) => Math.floor(Math.random() * (max - min + 1) - min);
 
   shuffle = (arr: string[]): string[] => arr.sort(() => +Math.random - 0.5);
+  
+  getSelectedKeys(config: IPasswordConfig): string[] {
+    return Object.entries(config).filter((ent) => ent[1]).map((ent) => ent[0]);
+  }
 }
